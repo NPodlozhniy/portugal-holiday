0bcd027a7f2099d7640b0120a1a1447eddb5aa91
NPodlozhniy <n.podlozhniy@zelf.co>
2023-01-08 03:38:02 +0000

    Add command-line argument

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import argparse
+
 from model import Calendar
+
+parser = argparse.ArgumentParser(
+    description="Calculate holiday calendar", epilog="Have a nice holiday!"
+)
+parser.add_argument(
+    "--year",
+    action="store",
+    help="specify the year (current year by default)",
+    metavar="<YOUR YEAR>",
+    nargs="?",
+    type=int,
+)
+args = parser.parse_args()
 
 if __name__ == "__main__":
 
-    calendar = Calendar.from_today()
-    print(calendar, end="\n\n")
+    if args.year:
+        calendar = Calendar(args.year)
+    else:
+        calendar = Calendar.from_today()
+
+    print(f"\n{calendar}\n")
 
     for date, holiday in calendar.public_holidays().items():
         print(f"{date}: {holiday}")
